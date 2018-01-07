05b144f476575ddca814b6a8bd886ea775251140
Hoon Lee <yl.hoony@gmail.com>
2018-01-07 09:30:29 +0000

    automatically backed up by learn

--- db/migrate/20180107092733_create_dogs.rb
+++ db/migrate/20180107092733_create_dogs.rb
@@ -1,4 +1,11 @@
 class CreateDogs < ActiveRecord::Migration
-  def change
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+ 
+  def down
   end
 end
